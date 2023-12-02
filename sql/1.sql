09725a68caf1ffbf80b6f5220d323b845db3dc18
Denis Voitenko <denis@denvot.dev>
2023-12-02 12:10:05 +0300

    Update 1.sql

--- sql/1.sql
+++ sql/1.sql
@@ -1,5 +1,2 @@
--- Результат: 50
-
-SELECT COUNT(*) FROM
-    (SELECT profile_id FROM post
-    INTERSECT SELECT profile_id FROM profile) as intersected
+SELECT COUNT(*) FROM (profile LEFT JOIN public.post p on profile.profile_id = p.profile_id) AS joined
+WHERE joined.post_id IS NULL
