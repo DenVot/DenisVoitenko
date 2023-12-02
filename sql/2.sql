6096af8f98b7c3e7a5932586f039f921904bb692
Denis Voitenko <denis@denvot.dev>
2023-12-02 13:15:37 +0300

    Update 2.sql

--- sql/2.sql
+++ sql/2.sql
@@ -1,6 +1,4 @@
--- Результат: 10
-
-SELECT COUNT(*) FROM post
+SELECT post_id FROM post
 WHERE ((SELECT COUNT(*) FROM comment WHERE post_id = post.post_id) = 2) AND
     LEFT(title, 1) ~ '^[0-9]+$' AND
     LENGTH(content) > 20
