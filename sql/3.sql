7ec2d4f99a148743cd396cbe3f9161c13c9d2246
Denis Voitenko <denis@denvot.dev>
2023-12-10 01:10:46 +0300

    Update 3.sql

--- sql/3.sql
+++ sql/3.sql
@@ -1,4 +1,7 @@
-SELECT post_id FROM post
-WHERE (SELECT COUNT(*) FROM comment WHERE post_id = post.post_id) <= 1
-ORDER BY post_id ASC 
+SELECT post.post_id
+FROM post
+WHERE (SELECT COUNT(*) FROM
+                            (SELECT c.post_id FROM post LEFT OUTER JOIN public.comment c on post.post_id = c.post_id) AS p2
+                        WHERE post.post_id = p2.post_id) <= 1
+ORDER BY post_id ASC
 LIMIT 10
